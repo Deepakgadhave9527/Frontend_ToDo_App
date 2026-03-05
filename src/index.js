acce1b20cd82e19e9aae24249968bf39499a613a
Deepak Gadhave <91776078+Deepakgadhave9527@users.noreply.github.com>
2026-03-05 11:29:09 +0530

    AI Fix Applied: RSQ-001

--- src/index.js
+++ src/index.js
@@ -4,17 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+import { createRoot } from 'react-dom/client';
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
